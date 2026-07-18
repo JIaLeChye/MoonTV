f2c8245b8ee2b5d7539173da57ab8b7e2e8895c9
unknown <jialecjl2016@outlook.com>
2026-07-19 00:14:07 +0800

    test: added upgrade to commitlint
     added the keyword upgrade

--- commitlint.config.js
+++ commitlint.config.js
@@ -8,6 +8,7 @@ module.exports = {
       'always',
       [
         'feat',
+        'upgrade',
         'fix',
         'docs',
         'chore',
--- src/lib/version.ts
+++ src/lib/version.ts
@@ -2,7 +2,7 @@
 
 'use client';
 
-const CURRENT_VERSION = '20260719000959';
+const CURRENT_VERSION = '20260719001408';
 
 // 导出当前版本号供其他地方使用
 export { CURRENT_VERSION };
